a0817205cb5e76892a06f8659e6e7238ed440822
MasterSJT <Joseph2.Dasilva@live.uwe.ac.uk>
2017-01-20 22:22:31 +0000

    Basic char movement
    Character moves

---
+++ Game/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    bool echo
+    {
+        get { return Input.GetKeyDown("e"); }
+    }
+    bool left
+    {
+        get { return Input.GetKey("a"); }
+    }
+    bool right
+    {
+        get { return Input.GetKey("d"); }
+    }
+    bool up
+    {
+        get { return Input.GetKey("w"); }
+    }
+    bool down
+    {
+        get { return Input.GetKey("s"); }
+    }
+    float speed;
+    Vector3 move;
+	// Use this for initialization
+	void Start () {
+        speed = 1.0f;
+        move = new Vector3(0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (echo)
+        {
+            // interact code here
+        }
+        if (left)
+        {
+            move = new Vector3(-1, 0);
+        }
+        else if (right)
+        {
+            move = new Vector3(1, 0);
+        }
+        else if (up)
+        {
+            move = new Vector3(0, 1);
+        }
+        else if (down)
+        {
+            move = new Vector3(0, -1);
+        }
+        else
+        {
+            move = new Vector3(0, 0);
+        }
+        this.transform.position += move * speed * Time.deltaTime;
+    }
+}
