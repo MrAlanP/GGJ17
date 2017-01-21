ab15856333b8c98ab20a4f52fe3d66b382a462f9
Alan Parsons <Toby2.Misselbrook@live.uwe.ac.uk>
2017-01-21 15:03:55 +0000

    - Z ordering for tiles

---
+++ Game/Assets/Scripts/Utility/OrderSpriteByZ.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrderSpriteByZ : MonoBehaviour {
+    SpriteRenderer spriteRenderer;
+
+    public bool updateOrder = false;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = (int)(gameObject.transform.position.z * -1000);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
