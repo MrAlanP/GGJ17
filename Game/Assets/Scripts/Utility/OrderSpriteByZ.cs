34b905de7f35686f492ca10a87e1dc4ddf5d01e2
MasterSJT <Toby2.Misselbrook@live.uwe.ac.uk>
2017-01-22 11:33:46 +0000

    Render order for walls and players

--- Game/Assets/Scripts/Utility/OrderSpriteByZ.cs
+++ Game/Assets/Scripts/Utility/OrderSpriteByZ.cs
@@ -5,15 +5,25 @@ public class OrderSpriteByZ : MonoBehaviour {
     SpriteRenderer spriteRenderer;
 
     public bool updateOrder = false;
+    public int orderOffset = 0;
 
 	// Use this for initialization
 	void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        SetOrder();
         //spriteRenderer.sortingOrder = (int)(gameObject.transform.position.z * -1000) + 10000;
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (updateOrder)
+        {
+            SetOrder();
+        }
 	}
+
+    void SetOrder()
+    {
+        spriteRenderer.sortingOrder = (int)(gameObject.transform.position.z * -100) + 1000 + orderOffset;
+    }
 }
