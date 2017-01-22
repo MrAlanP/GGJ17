1da21f47ed0091b90d347db9859975556a6bca19
Ellis Jackson <Nathan2.Butt@live.uwe.ac.uk>
2017-01-22 08:55:55 +0000

    stuff player dweath
    unknow stuff
    player now dies yay(?)

--- Game/Assets/Scripts/Managers/PlayerManager.cs
+++ Game/Assets/Scripts/Managers/PlayerManager.cs
@@ -41,6 +41,7 @@ public class PlayerManager : Singleton<PlayerManager> {
     public bool inObject;
     public bool action;
     public bool fire;
+    public bool alive;
     float speed;
     float baseSpeed;
     float sprintMax;
@@ -63,102 +64,107 @@ public class PlayerManager : Singleton<PlayerManager> {
         action = false;
         fire = false;
         inObject = false;
+        alive = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (echo)
+        if (alive)
         {
-            action = true;
-        }
-        else
-        {
-            action = false;
-        }
-        //Debug.Log(action);
-        if (foxtrot)
-        {
-            fire = true;
-        }
-        else
-        {
-            fire = false;
-        }
-        // movement controlls
-        if (left)
-        {
-            x = -1;
-            animator.SetInteger("direction", -90);
-        }
-        else if (right)
-        {
-            x = 1;
-            animator.SetInteger("direction", 90);
-        }
-        else
-        {
-            x = 0;
-        }
-
-        if (up)
-        {
-            z = 1;
-        }
-        else if (down)
-        {
-            z = -1;
-        }
-        else
-        {
-            z = 0;
-        }
-
-        // apply sprint speed if enoght sprint energy (sprintCurrent) is left
-        if (sprint && sprintCurrent > 0.0f && !cooldown)
-        {
-            speed = sprintingSpeed;
-            sprintCurrent -= 1.5f * Time.deltaTime;
-        }
-        else
-        {
-            speed = baseSpeed;
-            if (sprintCurrent < sprintMax)
+            if (echo)
             {
-                sprintCurrent += 0.25f * Time.deltaTime;
+                action = true;
             }
-        }
+            else
+            {
+                action = false;
+            }
+            //Debug.Log(action);
+            if (foxtrot)
+            {
+                fire = true;
+            }
+            else
+            {
+                fire = false;
+            }
+            // movement controlls
+            if (left)
+            {
+                x = -1;
+                animator.SetInteger("direction", -90);
+            }
+            else if (right)
+            {
+                x = 1;
+                animator.SetInteger("direction", 90);
+            }
+            else
+            {
+                x = 0;
+            }
 
-        if (cooldown && sprintCurrent >= 0.5f)
-        {
-            cooldown = false;
-        }
+            if (up)
+            {
+                z = 1;
+            }
+            else if (down)
+            {
+                z = -1;
+            }
+            else
+            {
+                z = 0;
+            }
 
-        if (sprintCurrent >= sprintMax)
-        {
-            sprintCurrent = sprintMax;
-        }
-        else if (sprintCurrent < 0.0f)
-        {
-            sprintCurrent = 0.0f;
-            cooldown = true;
-        }
-        
-        // applys movement to player
-        move = new Vector3(x, 0, z);
-        if (!inObject)
-        {
-            rb.velocity = new Vector3(move.x, move.y, move.z) * speed;
-        }
-        else
-        {
-            rb.position = moveToObject;
+            // apply sprint speed if enoght sprint energy (sprintCurrent) is left
+            if (sprint && sprintCurrent > 0.0f && !cooldown)
+            {
+                speed = sprintingSpeed;
+                sprintCurrent -= 1.5f * Time.deltaTime;
+            }
+            else
+            {
+                speed = baseSpeed;
+                if (sprintCurrent < sprintMax)
+                {
+                    sprintCurrent += 0.25f * Time.deltaTime;
+                }
+            }
+
+            if (cooldown && sprintCurrent >= 0.5f)
+            {
+                cooldown = false;
+            }
+
+            if (sprintCurrent >= sprintMax)
+            {
+                sprintCurrent = sprintMax;
+            }
+            else if (sprintCurrent < 0.0f)
+            {
+                sprintCurrent = 0.0f;
+                cooldown = true;
+            }
+
+            // applys movement to player
+            move = new Vector3(x, 0, z);
+            if (!inObject)
+            {
+                rb.velocity = new Vector3(move.x, move.y, move.z) * speed;
+            }
+            else
+            {
+                rb.position = moveToObject;
+            }
         }
     }
 
     //What happens when the Player is hit by Swat
     public void OnDeath()
     {
+        alive = false;
+        animator.SetBool("alive", false);
     }
 
 }
